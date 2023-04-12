fc13288b92da86098e7199aac96588525d936d15
Melvin Bulan <melvin.bulan000@gmail.com>
2023-04-12 23:55:52 +0800

    Add navbar and aboutus components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { React } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import AboutUs from './components/AboutUs'
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <AboutUs />
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,141 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import CssBaseline from '@mui/material/CssBaseline';
-import useScrollTrigger from '@mui/material/useScrollTrigger';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import logo from '../img/logo.png';
+import bg from '../img/bg.png';
+import Stack from '@mui/material/Stack';
 
-function ElevationScroll(props) {
-  const { children, window } = props;
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
-  const trigger = useScrollTrigger({
-    disableHysteresis: true,
-    threshold: 0,
-    target: window ? window() : undefined,
-  });
+const drawerWidth = 240;
+const navItems = ['Home', 'About Us', 'Join Us', 'Contact Us'];
 
-  return React.cloneElement(children, {
-    elevation: trigger ? 4 : 0,
-  });
+function Navbar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+  
+  const isMobile = useMediaQuery('(max-width:600px)');
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center'}}>
+      <img src={logo} alt="MUI logo" style={{width: '100px', height: 'auto', paddingTop: '2rem'}}/>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} >
+            <ListItemButton sx={{ textAlign: 'center' }} >
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        maxWidth: '80%',
+        margin: '0 auto',
+      }}>
+      <CssBaseline />
+      
+      <AppBar component="nav" sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        margin: '1rem auto',
+        paddingTop: '1rem',
+        backgroundColor: '#0000',
+      }} >
+
+        
+        <Toolbar sx={{
+        display: 'flex',
+        maxWidth: '90%',
+        margin: '0 3rem',
+        justifyContent: "flex-start",
+      }}>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+            
+             {isMobile ? null : (
+             <Stack
+            direction="row"
+            justifyContent="space-around"
+            alignItems="center"
+            spacing={2}
+            sx = {{width: '100%',}}
+            >
+         
+
+          <Box >
+            <img src={logo} alt="MUI logo" style={{width: '200px', height: '50px'}}/>
+          </Box>
+          <Box>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: '#000000', paddingLeft: '3rem', '&:hover': {color: '#000000', fontWeight: 'bold'}}}>
+                {item}
+              </Button>
+            ))}
+          </Box>
+         
+          </Stack>
+           )}
+        </Toolbar>
+      </AppBar>
+      
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box component="main" sx={{width: '99vw'}}>
+        <Toolbar />
+        <img src={bg} alt="Hero section" style={{paddingTop: '2rem', backgroundSize: 'cover', width: '100%', height: '90vh'}} />
+      </Box>
+    </Box>
+  );
 }
 
-ElevationScroll.propTypes = {
-  children: PropTypes.element.isRequired,
+Navbar.propTypes = {
   /**
    * Injected by the documentation to work in an iframe.
    * You won't need it on your project.
@@ -33,32 +143,4 @@ ElevationScroll.propTypes = {
   window: PropTypes.func,
 };
 
-export default function Navbar(props) {
-  return (
-    <React.Fragment>
-      <CssBaseline />
-      <ElevationScroll {...props}>
-        <AppBar>
-          <Toolbar>
-            <Typography variant="h6" component="div">
-              Scroll to elevate App bar
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </ElevationScroll>
-      <Toolbar />
-      <Container>
-        <Box sx={{ my: 2 }}>
-          {[...new Array(12)]
-            .map(
-              () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-            )
-            .join('\n')}
-        </Box>
-      </Container>
-    </React.Fragment>
-  );
-}
+export default Navbar;
